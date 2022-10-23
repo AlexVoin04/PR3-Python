07d68597f3cb5b409b3b1b56f1aa08d00d556893
Alex <alexiv.04@mail.ru>
2022-10-23 15:03:07 +0700

    3 commit

--- main.py
+++ main.py
@@ -11,41 +11,52 @@ def rand(n, num):
     rw = RandomWord()
     rw.constant_word_size = False
     filename = f"./result_files/Process-{num}-{os.getpid()}.txt"
-    with open(filename, "a") as file:
-        for i in range(n):
-            file.write(f'{rw.generate()}\n')
-    get_analiz1(filename)
-    get_analiz2(filename)
+    try:
+        with open(filename, "a") as file:
+            for i in range(n):
+                file.write(f'{rw.generate()}\n')
+        get_analiz1(filename)
+        get_analiz2(filename)
+    except Exception:
+        pass
+
+
 
 
 if __name__ == '__main__':
     manager = multiprocessing.Manager()
 
-    list_process: List[Process] = []
+    try:
+        list_process: List[Process] = []
 
-    for i in range(1, multiprocessing.cpu_count() + 1):
-        name_pr: str = f'pr{i}'
-        r: int = random.randint(100000, 5000000)
-        process = Process(target=rand, args=(r, i), name=name_pr)
-        list_process.append(process)
-        process.start()
+        for i in range(1, multiprocessing.cpu_count() + 1):
+            name_pr: str = f'pr{i}'
+            r: int = random.randint(100000, 5000000)
+            process = Process(target=rand, args=(r, i), name=name_pr)
+            list_process.append(process)
+            process.start()
 
-    for i in range(1, multiprocessing.cpu_count() + 1):
-        list_process[i].join()
+        for i in range(1, multiprocessing.cpu_count() + 1):
+            list_process[i].join()
+    except Exception:
+        pass
+
+
+
 
 
 def get_min(file):
     m = ""
     with open(f'{file}', 'r') as file1:
         m: str = min(file1, key=len)
-    return len(m)
+    return len(m)-1
 
 
 def get_max(file):
     m: str = ""
     with open(f'{file}', 'r') as file1:
         m: str = max(file1, key=len)
-    return len(m)
+    return len(m)-1
 
 
 def get_all(file):
@@ -113,13 +124,15 @@ def get_analiz1(file):
                  f"  4. Средняя длина слова --> {get_sred(file)}\n"
                  f"  5. Количество гласных --> {listr1[0]}\n"
                  f"  6. Количсетво согласных --> {listr1[1]}\n"
-                 f"  7. Количество повторений слов с одинаковой длиной:\n\n")
+                 f"  7. Количество повторений слов с одинаковой длиной:\n")
 
 
 def get_analiz2(file):
     listr2 = get_sl(file)
-    for i in range(len(listr2)):
-        return print(f"   * {i + 1} сим. >> {listr2[i]} повтор.")
+    s: str = ""
+    for i in range(1, len(listr2)):
+        s += f"     * {i} сим. >> {listr2[i]} повтор.\n"
+    return print(s)
 
 
 def get_sl(file):
@@ -158,17 +171,17 @@ def get_sl(file):
                     sl10 += 1
     return [sl1, sl2, sl3, sl4, sl5, sl6, sl6, sl7, sl8, sl9, sl10]
 
-def get_sl_all(file):
-    dict = {'1': 1,
-            '2': 2,
-            '3': 3,
-            '4': 4,
-            '5': 5,
-            '6': 6,
-            '7': 7,
-            '8': 8,
-            '9': 9,
-            '10': 0}
-    with open(f'{file}', 'r') as file1:
-        for line in file1.readlines():
-            if len(line) == dict.get()
+#def get_sl_all(file):
+ #   dict = {'1': 1,
+ #           '2': 2,
+ #           '3': 3,
+ #           '4': 4,
+ #           '5': 5,
+ #           '6': 6,
+ #           '7': 7,
+ #           '8': 8,
+ #           '9': 9,
+ #           '10': 0}
+ #   with open(f'{file}', 'r') as file1:
+ #       for line in file1.readlines():
+ #           if len(line) == dict.get()
